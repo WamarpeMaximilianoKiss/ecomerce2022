4efc3bc941e492dc8870a98cf9d3f8c6fbe780bb
WamarpeMaximilianoKiss <maxiks_92@hotmail.com>
2022-01-28 17:29:20 -0300

    link to checkout

--- src/components/Total.js
+++ src/components/Total.js
@@ -3,6 +3,9 @@ import accounting from 'accounting';
 import { Button, makeStyles } from '@material-ui/core';
 import { useStateValue } from '../StateProvider';
 import { getBasketTotal } from '../reducer';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const useStyle = makeStyles((theme) => ({
     root:  {
@@ -17,15 +20,20 @@ const useStyle = makeStyles((theme) => ({
     }
 }))
 
+
 const Total = () => {
     const classes = useStyle();
     const [{basket}, dispatch] = useStateValue();
-
+    const navigate = useNavigate(); 
+    const toCheckOut = () =>{
+        navigate('/checkout')
+    }
+    
   return (
       <div className={classes.root} > 
         <h5>Total items: {basket?.length}</h5>
         <h5>{accounting.formatMoney(getBasketTotal(basket),"$")}</h5>
-        <Button className={classes.button} variant="contained" color="secondary" >Check out</Button>
+        <Button className={classes.button} variant="contained" color="secondary" onClick={toCheckOut} >Check out</Button>
 
      </div>
   )
