6fc76f26c890873c6b95a2c6ffb3945cd43586d2
WamarpeMaximilianoKiss <maxiks_92@hotmail.com>
2022-01-28 17:18:27 -0300

    Checkout desde cero

---
+++ src/components/ChekoutForm/styles.js
@@ -0,0 +1,43 @@
+import { AppBar, makeStyles } from "@material-ui/core";
+
+
+export default makeStyles ((theme) => ({
+    appBar:{
+        position: "relative",
+    },
+    layout: {
+        width: "auto",
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+        [
+            theme.breakpoints.up(600 + theme.spacing(2) * 2)
+        ]:{
+            width: 600,
+            marginLeft: "auto",
+            marginRight: "auto"
+        },
+    },
+    papper: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2),
+        [
+            theme.breakpoints.up(600 + theme.spacing(3) * 2)
+        ]:{
+            marginTop: theme.spacing(6),
+            marginBottom: theme.spacing(6),
+            padding: theme.spacing(3),
+        },
+    },
+    stepper:{
+        padding: theme.spacing(3, 0, 5),
+    },
+    buttons: {
+        display: "flex",
+        justifyContent: "flex-end",
+    },
+    button: {
+        marginTop: theme.spacing(3),
+        marginLeft: theme.spacing(1),
+    },
+}));
