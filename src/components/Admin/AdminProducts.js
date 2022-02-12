430e554a27333c3c230fc3757ee69a43552bde02
WamarpeMaximilianoKiss <maxiks_92@hotmail.com>
2022-02-12 06:04:19 -0300

    productos leyendo desde api

--- src/components/Admin/AdminProducts.js
+++ src/components/Admin/AdminProducts.js
@@ -1,62 +1,16 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import MaterialTable from 'material-table'
-
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
 
 const columns = [
     { field: 'id', title: 'Id', type: "numeric" },
-    { field: 'name', title: 'Nombre' },
-    { field: 'productType', title: 'Categoria' },
-    { field: 'weight', title: 'Peso', type: "numeric" },
-    { field: 'measure_sale', title: 'Medida de venta' },
+    { field: 'nombre', title: 'Nombre' },
+    { field: 'categoria', title: 'Categoria' },
+    { field: 'peso', title: 'Peso', type: "numeric" },
     { field: 'stock', title: 'Stock', type: "numeric" },
-    { field: 'stock_min', title: 'Stock mínimo', type: "numeric" },
-    { field: 'price_cost', title: 'Precio de costo', type: "numeric" },
-    { field: 'price_sale', title: 'Precio de venta', type: "numeric" },
-    { field: 'enabled', title: 'Habilitado', align: "right" }
+    { field: 'importe_venta', title: 'Precio de venta', type: "numeric" },
+    { field: 'habilitado', title: 'Habilitado', align: "right" }
 ];
-
-
-const rows = [
-    {
-        "id": 1, "name": "Tobillera", "productType": "Pulseras", "weight": 50, "measure_sale": "GRS", "stock": 50,
-        "stock_min": 150, "price_cost": 80.5, "price_sale": 160, "enabled": "SI"
-    },
-    {
-        "id": 2, "name": "Piedra de caca", "productType": "Piedras", "weight": 50, "measure_sale": "GRS", "stock": 33,
-        "stock_min": 150, "price_cost": 140, "price_sale": 360, "enabled": "SI"
-    },
-    {
-        "id": 3, "name": "Mostasilla batman", "productType": "Mostasillas", "weight": 50, "measure_sale": "GRS", "stock": 123,
-        "stock_min": 50, "price_cost": 40, "price_sale": 60, "enabled": "SI"
-    },
-    {
-        "id": 4, "name": "Mostasilla barbie", "productType": "Mostasillas", "weight": 50, "measure_sale": "GRS", "stock": 40,
-        "stock_min": 150, "price_cost": 40, "price_sale": 65, "enabled": "SI"
-    },
-
-
-];
-
-
-
-const useStyles = makeStyles({
-    root: {
-        width: '100%',
-    },
-    mnuGroupButtons: {
-        marginRight: "20px",
-    },
-    container: {
-        maxHeight: 550,
-    },
-    btnGroupButtons: {
-        marginBottom: 25,
-        fontSize: "large",
-        fontFamily: "Arial",
-        height: 55,
-    }
-});
 
 const editarProducto = (data) => {
     alert("editar " + data.name)
@@ -67,10 +21,37 @@ const eliminarProducto = (data) => {
 }
 
 
+
 const AdminProducts = () => {
+
+    const baseURL = "https://apiplayabrava.herokuapp.com/api/productos/productos";
+    const [products, setProducts] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
+            setProducts(response.data);
+            console.log("Productos :", response.data)
+        });
+    }, []);
+
+    console.log(products)
     return (
-        <div>
-            <MaterialTable columns={columns} data={rows} title="Productos"
+
+        < div >
+            <MaterialTable columns={columns}
+                data={query =>
+                    new Promise((resolve, reject) => {
+                        fetch(baseURL)
+                            .then(response => response.json())
+                            .then(result => {
+                                console.log("Resultadito: ", result)
+                                resolve({
+                                    data: result
+                                })
+                            })
+                    })
+                }
+                title="Productos"
                 options={{
                     headerStyle: {
                         backgroundColor: '#01579b',
@@ -78,7 +59,8 @@ const AdminProducts = () => {
                     },
                     actionsColumnIndex: -1,
                     exportButton: true,
-                    grouping: true
+                    grouping: true,
+
                 }}
                 actions={[
                     {
@@ -90,6 +72,12 @@ const AdminProducts = () => {
                         icon: "delete",
                         tooltip: "Eliminar producto",
                         onClick: (event, rowData) => eliminarProducto(rowData)
+                    },
+                    {
+                        icon: 'add',
+                        tooltip: 'Agregar nuevo Producto',
+                        isFreeAction: true,
+                        onClick: (event) => alert("You want to add a new row")
                     }
                 ]}
                 localization={{
@@ -118,13 +106,11 @@ const AdminProducts = () => {
                         nextTooltip: "Siguiente",
                         labelDisplayedRows: "{from}-{to} de {count}",
                         labelRowsSelect: "Filas"
-
                     }
-
                 }}
-            />
-        </div>
 
+            />
+        </div >
     );
 }
 
