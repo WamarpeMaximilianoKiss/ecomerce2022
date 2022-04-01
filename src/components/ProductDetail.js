dd4b0ca5f12190afff0aadeb822a3d35b2d0165e
WamarpeMaximilianoKiss <maxiks_92@hotmail.com>
2022-03-31 23:40:35 -0300

    detalle de producto responsive

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -29,8 +29,6 @@ import { actionType } from '../reducer';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        flexGrow: 1,
-        margin: 20,
         backgroundColor: "whitesmoke",
         borderRadius: 10
     },
@@ -38,11 +36,9 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-        minWidth: 350
     },
     media: {
         height: 0,
-        paddingTop: '56.25%', // 16:9
     },
     expand: {
         transform: 'rotate(0deg)',
@@ -55,23 +51,18 @@ const useStyles = makeStyles((theme) => ({
         transform: 'rotate(180deg)',
     },
     thumbnail: {
-        alignItems: "center",
-        textAlign: "center",
+
     },
     thumbnails: {
         alignItems: "center",
         textAlign: "center",
     },
     cartaEntera: {
-        display: "flex",
-
     },
     imagen: {
         width: "100%",
-        minHeight: 298,
-        maxHeight: 380,
-        margin: 20,
-
+        textAlign: "center",
+        alignItems: "center",
     },
     imageList: {
         width: 200,
@@ -95,15 +86,12 @@ const useStyles = makeStyles((theme) => ({
     },
     boton: {
         width: 200,
-        marginBlock: 10,
     },
     cartaPrecio: {
         backgroundColor: "#eceff1",
         alignItems: "center",
         textAlign: "center",
-        margin: 20,
         borderRadius: 10,
-        minWidth: 300,
     },
     cardheader: {
         backgroundColor: "#455a64",
@@ -126,10 +114,13 @@ const useStyles = makeStyles((theme) => ({
 
     },
     lblCantidad: {
-        marginInline: 20,
         alignItems: "center",
         textAlign: "center",
+    },
+    cantFotos: {
+        display: "flex",
 
+        fontSize: "2.4rem",
     },
 
 }));
@@ -169,150 +160,141 @@ export default function ProductDetail() {
 
     return (
         <div className={classes.root}>
-            <Container>
-                <Grid spacing={1} className={classes.cartaEntera} >
-                    <Grid lg={2} >
-                        <br></br>
-                        <ImageList rowHeight={120} className={classes.imageList} cols={1}>
-                            {productDetail[0].imagenes.map((item, i) => (
-                                <ImageListItem key={i} cols={1}>
-                                    <img src={item.link_imagen} />
-                                </ImageListItem>
-                            ))}
-                        </ImageList>
-                    </Grid>
-                    <Grid item lg={1} md={1} sm={1} />
+            <Grid container spacing={2}>
+                <Grid item lg={8} md={7} sm={7} xs={12}>
+                    <div className={classes.imagen}><img src={productDetail[0].imagenes[0].link_imagen} width="100%"></img></div>
+                    <div className={classes.thumbnail}>
+                        <div className={classes.cantFotos} >
+                            <img src={productDetail[0].imagenes[0].link_imagen} width="20%"></img>
+                            <img src={productDetail[0].imagenes[0].link_imagen} width="20%"></img>
+                            <img src={productDetail[0].imagenes[0].link_imagen} width="20%"></img>
+                            <img src={productDetail[0].imagenes[0].link_imagen} width="20%"></img>
+                            <div>+21</div>
+                        </div>
+                    </div>
+                </Grid>
+                <Grid item lg={3} md={4} sm={5} xs={12} >
+                    <Card className={classes.cartaPrecio} >
+                        <CardContent className={classes.cardheader}  >
+                            <Typography className={classes.title} color="textSecondary" gutterBottom>
+                                {productDetail[0].nombre}
+                            </Typography>
+                            <Typography variant="h3" component="h3">
+                                {accounting.formatMoney(productDetail[0].importe_venta)}
+                            </Typography>
+                            <Typography className={classes.pos} color="textSecondary">
+                                <Chip
+                                    label="Ultimos 13"
+                                    clickable
+                                    color="secondary"
 
-                    <Grid lg={6} className={classes.cartaEntera}>
-                        <div className={classes.imagen}><img src={productDetail[0].imagenes[0].link_imagen} width="100%"></img></div>
-                    </Grid>
-                    <Grid item lg={1} md={1} sm={1} />
+                                />
+                            </Typography>
 
-                    <Grid lg={4} className={classes.cartaEntera}  >
+                        </CardContent>
+                        <Divider></Divider>
+                        <div className={classes.botones}>
+                            <Typography variant="h6" gutterBottom>
+                                Envio a domicilio: $560
+                                <br />
+                                Envio a sucursal: $360
+                            </Typography>
+                            <Typography variant='h6'>
+                                <Chip
+                                    deleteIcon={<DoneIcon />}
+                                    label=" Llevando 20 unidades envio gratis!" variant="outlined" />
 
-                        <Card className={classes.cartaPrecio} >
-                            <CardContent className={classes.cardheader}  >
-                                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                                    {productDetail[0].nombre}
-                                </Typography>
-                                <Typography variant="h3" component="h3">
-                                    {accounting.formatMoney(productDetail[0].importe_venta)}
-                                </Typography>
-                                <Typography className={classes.pos} color="textSecondary">
-                                    <Chip
-                                        label="Ultimos 13"
-                                        clickable
-                                        color="secondary"
+                            </Typography>
+                            <div className={classes.cantidades}>
 
-                                    />
-                                </Typography>
-
-                            </CardContent>
-                            <Divider></Divider>
-                            <div className={classes.botones}>
-                                <Typography variant="h6" gutterBottom>
-                                    Envio a domicilio: $560
-                                    <br />
-                                    Envio a sucursal: $360
-                                </Typography>
-                                <Typography variant='h6'>
-                                    <Chip
-                                        deleteIcon={<DoneIcon />}
-                                        label=" Llevando 20 unidades envio gratis!" variant="outlined" />
-
-                                </Typography>
-                                <div className={classes.cantidades}>
-
-                                    <Button variant="contained" color="primary" size="small" className={classes.btnCantidades} onClick={removeItem}>
-                                        <RemoveIcon />
-                                    </Button>
-                                    <Typography component="div" variant="h2" className={classes.lblCantidad} >
-                                        {productDetail[0].cantidad}
-                                    </Typography>
-                                    <Button variant="contained" color="primary" size="small" className={classes.btnCantidades} onClick={addToBasket}>
-                                        <AddIcon />
-                                    </Button>
-
-                                </div>
-                                <Button className={classes.boton} variant="contained" color="primary">
-                                    Comprar
+                                <Button variant="contained" color="primary" size="small" className={classes.btnCantidades} onClick={removeItem}>
+                                    <RemoveIcon />
                                 </Button>
-                                <br></br>
-                                <Button className={classes.boton} variant="outlined" color="secondary">
-                                    Agregar al carrito
+                                <Typography component="div" variant="h2" className={classes.lblCantidad} >
+                                    {productDetail[0].cantidad}
+                                </Typography>
+                                <Button variant="contained" color="primary" size="small" className={classes.btnCantidades} onClick={addToBasket}>
+                                    <AddIcon />
                                 </Button>
+
                             </div>
-                        </Card>
-
-                    </Grid>
+                            <Button className={classes.boton} variant="contained" color="primary">
+                                Comprar
+                            </Button>
+                            <br></br>
+                            <Button className={classes.boton} variant="outlined" color="secondary">
+                                Agregar al carrito
+                            </Button>
+                        </div>
+                    </Card>
                 </Grid>
-                <Grid>
-                    <Grid item lg={12}>
-                        <Card className={classes.root} >
-                            <CardHeader
+            </Grid>
+            <Grid container spacing={6} >
+                <Grid item lg={6} md={4} sm={12} xs={12} >
+                    <Card className={classes.root} >
+                        <CardHeader
 
-                                action={
-                                    <IconButton aria-label="settings">
-                                        <MoreVertIcon />
-                                    </IconButton>
-                                }
-                                title={productDetail[0].nombre}
+                            action={
+                                <IconButton aria-label="settings">
+                                    <MoreVertIcon />
+                                </IconButton>
+                            }
+                            title={productDetail[0].nombre}
 
-                            />
-                            <CardMedia>
-                            </CardMedia>
+                        />
+                        <CardMedia>
+                        </CardMedia>
+                        <CardContent>
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                This impressive paella is a perfect party dish and a fun meal to cook together with your
+                                guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                            </Typography>
+                        </CardContent>
+                        <CardActions disableSpacing>
+                            <IconButton aria-label="share">
+                                <ShareIcon />
+                            </IconButton>
+                            <IconButton
+                                className={clsx(classes.expand, {
+                                    [classes.expandOpen]: expanded,
+                                })}
+                                onClick={handleExpandClick}
+                                aria-expanded={expanded}
+                                aria-label="show more"
+                            >
+                                <ExpandMoreIcon />
+                            </IconButton>
+                        </CardActions>
+                        <Collapse in={expanded} timeout="auto" unmountOnExit>
                             <CardContent>
-                                <Typography variant="body2" color="textSecondary" component="p">
-                                    This impressive paella is a perfect party dish and a fun meal to cook together with your
-                                    guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                                <Typography paragraph>Method:</Typography>
+                                <Typography paragraph>
+                                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                                    minutes.
+                                </Typography>
+                                <Typography paragraph>
+                                    Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                                    heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                                    browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                                    and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                                    pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                                    saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                                </Typography>
+                                <Typography paragraph>
+                                    Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                                    without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                                    medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                                    again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                                    minutes more. (Discard any mussels that don’t open.)
+                                </Typography>
+                                <Typography>
+                                    Set aside off of the heat to let rest for 10 minutes, and then serve.
                                 </Typography>
                             </CardContent>
-                            <CardActions disableSpacing>
-                                <IconButton aria-label="share">
-                                    <ShareIcon />
-                                </IconButton>
-                                <IconButton
-                                    className={clsx(classes.expand, {
-                                        [classes.expandOpen]: expanded,
-                                    })}
-                                    onClick={handleExpandClick}
-                                    aria-expanded={expanded}
-                                    aria-label="show more"
-                                >
-                                    <ExpandMoreIcon />
-                                </IconButton>
-                            </CardActions>
-                            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                                <CardContent>
-                                    <Typography paragraph>Method:</Typography>
-                                    <Typography paragraph>
-                                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                                        minutes.
-                                    </Typography>
-                                    <Typography paragraph>
-                                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                                        heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                                        browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-                                        and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                                        pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-                                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                                    </Typography>
-                                    <Typography paragraph>
-                                        Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-                                        without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-                                        medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-                                        again without stirring, until mussels have opened and rice is just tender, 5 to 7
-                                        minutes more. (Discard any mussels that don’t open.)
-                                    </Typography>
-                                    <Typography>
-                                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                                    </Typography>
-                                </CardContent>
-                            </Collapse>
-                        </Card>
-                    </Grid>
+                        </Collapse>
+                    </Card>
                 </Grid>
-            </Container>
+            </Grid>
 
         </div>
     );
